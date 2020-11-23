7f442cacfad7adc5172278bcc2a50e57fa958736
chautruongphat123 <chautruongphat123@gmail.com>
2020-11-23 22:07:05 +0700

    create database

---
+++ Nhom9_RentingDisk_XDPM/Business/CustomerBLL.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess;
+using DataAccess.DTO;
+using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class CustomerBLL
+    {
+        private CustomerDAL dal;
+        public CustomerBLL()
+        {
+            dal = new CustomerDAL();
+        }
+        public Result add(Customer customer)
+        {
+            return dal.add(customer);
+        }
+
+        public Result update(Customer customer)
+        {
+            return dal.update(customer);
+        }
+
+        public Result delete(int idCustomer)
+        {
+            return dal.delete(idCustomer);
+        }
+
+        public Customer searchCustomerbyId(string idCustomer)
+        {
+            return dal.searchCustomerbyId(idCustomer);
+        }
+    }
+}
